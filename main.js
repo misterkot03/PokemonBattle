f14b7d92d70e18feba0d68ad330153a96873d22d
Dima <zhilkin142510@gmail.com>
2023-10-07 21:48:58 +0800

    add damage and change hp

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 
+
+const btn = document.getElementById('btn-kick');
+const btnBig = document.getElementById('btn-kick-big');
+
+
 const mainHero = {
     name:'Squirtle',
     defaultHP: 100,
@@ -14,14 +19,47 @@ const enemyHero = {
     elHP:document.getElementById('health-enemy'),
     elProgressBar:document.getElementById('progressbar-enemy'),
 }
-
+function damage(count) {
+    changeHP(random(count),mainHero);
+    changeHP(random(count),enemyHero);
+}
+btn.addEventListener('click',function () {
+    damage(20);
+});
+btnBig.addEventListener('click',function () {
+    damage(50);
+});
 
 function init() {
     renderHP(mainHero);
     renderHP(enemyHero);
 }
 function  renderHP(person){
-    console.log(person.name+": "+person.elHP);
+    renderHPLife(person);
+    renderProgressbarHP(person);
+}
+
+function renderHPLife(person) {
+    console.log(person.elHP);
+    person.elHP.innerText = person.damageHP + ' / ' + person.defaultHP;
+}
+function renderProgressbarHP(person) {
+    person.elProgressBar.style.width = person.damageHP + '%';
+}
+function changeHP(count,person) {
+    if (person.damageHP < count){
+        person.damageHP =0;
+        alert('Проиграл - '+person.name);
+        btn.disabled = true;
+        btnBig.disabled = true;
+    }else{
+        person.damageHP -=count;
+    }
+
+    renderHP(person);
+}
+function random(num) {
+    return Math.ceil(Math.random() * num);
 }
 
 init();
